08f7a5ae2cbf352bae6409250db3a96b8b747b6e
aabdyshev <aabdyshev@nurtelecom.kg>
2023-04-19 09:57:25 +0600

    initial commit

---
+++ DeveloperAppleLessons/01-CreatingAndCombiningViews/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Abdyshev Amantay on 19/4/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
